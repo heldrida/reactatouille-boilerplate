54a6bc0cefb07a9efd57b8685ec7b1370bda4304
Helder <heldrida@gmail.com>
2016-10-03 10:46:24 +0100

    improvments in the server.js tested for herokuapp

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 var express = require('express'),
-  app = express(),
-  http = require('http'),
-  httpServer = http.Server(app),
-  port = process.env.port || 8080
+	path = require('path'),
+	app = express(),
+	http = require('http'),
+	httpServer = http.Server(app),
+	port = process.env.PORT ? process.env.PORT : 8080,
+	dist = path.join(__dirname, 'dist')
 
+app.use(express.static(dist));
 
-app.use(express.static(__dirname + '/dist'));
+app.get('*', function(req, res) {
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(path.join(dist, 'index.html'));
+
 });
 
-app.listen(port, function () {
-  console.log('App from `dist` listening on port ' + port + '!');
+app.listen(port, function (error) {
+
+	if (error) {
+		console.log(error); // eslint-disable-line no-console
+	}
+
+	console.info('Express is listening on port %s.', port); // eslint-disable-line no-console
+
 });
