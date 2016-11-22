6bab6d16424b8f2d946a9e6c0c9466c16cd5ca27
Helder <heldrida@gmail.com>
2016-11-22 01:41:56 +0000

    fix for the warning message following Dan Abramov suggestion (hacky)

--- src/js/root.js
+++ src/js/root.js
@@ -11,6 +11,22 @@ import { routerMiddleware, push } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import configureStore from './store'
 
+// https://github.com/gaearon/react-hot-boilerplate/pull/61#issuecomment-211504531
+Router.prototype.componentWillReceiveProps = function(nextProps) {
+  let components = [];
+  function grabComponents(element) {
+    // This only works for JSX routes, adjust accordingly for plain JS config
+    if (element.props && element.props.component) {
+      components.push(element.props.component);
+    }
+    if (element.props && element.props.children) {
+      React.Children.forEach(element.props.children, grabComponents);
+    }
+  }
+  grabComponents(nextProps.routes || nextProps.children);
+  components.forEach(React.createElement); // force patching
+};
+
 // include the stylesheet entry point
 require('../sass/app.scss');
 
