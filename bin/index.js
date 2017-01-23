c57954c758a7cd3818c877d55c2751acf179f4b8
Helder <heldrida@gmail.com>
2017-01-23 11:24:29 +0000

    Created the commands to copy the template files to the desired project directory

--- bin/index.js
+++ bin/index.js
@@ -5,10 +5,16 @@ require('babel-polyfill')
 var spawn = require('child_process').spawn,
 	chalk = require('chalk'),
 	figlet = require('figlet'),
-	program = require('commander')
+	program = require('commander'),
+	Promise = require("bluebird"),
+	fs = Promise.promisifyAll(require('fs-extra')),
+	rootDir = __dirname.replace(/\/bin/, ''),
+	projectName = ''
 
+// Clear the screen
 spawn('clear', [null], { stdio: 'inherit' })
 
+// Show the startup banner
 console.log(
 	chalk.magenta(
 		figlet.textSync('Reacstart', { horizontalLayout: 'full' })
@@ -19,15 +25,41 @@ console.log(
 	'\n'
 )
 
+// Set command line program parameters
 program
 	.version(require('../package.json').version)
-	.usage('[options] [project name]')
+	.usage('[project name]')
 	.parse(process.argv);
 
-var pname = program.args[0]
+// Set the name
+projectName = program.args[0]
 
-if (!pname) {
-	program.help();
+// Show help if user did not provide a project name
+if (!projectName) {
+
+	program.help()
+
 } else {
-	gs(pname);
+
+	// 
+	fs.copyAsync(rootDir + '/template', projectName, { clobber: true })	
+		.then(function (err) {
+
+			// Show the initialisation text, white space added to text align
+			console.log(chalk.blue(' ' + 'Creating the project directory `' + projectName + '`...'))			
+			console.log('\n')
+
+			if (err) {
+
+				return console.error(chalk.red.bold(err))
+
+			} else {
+				
+				console.log(chalk.green(' ' + 'Success! Your project boilerplate is ready! Happy coding yo!'))
+				console.log('\n')
+
+			}
+
+		})
+
 }
