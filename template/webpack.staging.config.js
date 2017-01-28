11b0ecd0ae0068f4c7f670db16bf921ea4176604
Helder <heldrida@gmail.com>
2017-01-28 18:38:53 +0000

    created the webpack production config file

--- template/webpack.staging.config.js
+++ template/webpack.staging.config.js
@@ -34,7 +34,7 @@ module.exports = {
 			{
 				test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
 				use: [
-					'file-loader'
+					'file-loader?name=[path][name].[ext]&outputPath=/assets/fonts/&publicPath=/assets/fonts/'
 				]
 			},
 			{
