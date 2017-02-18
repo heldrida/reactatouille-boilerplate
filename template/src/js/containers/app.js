ed9348e782aec6a255d421eb0168ee5994a02795
Helder <heldrida@gmail.com>
2017-02-18 22:11:53 +0000

    package updates

--- template/src/js/containers/app.js
+++ template/src/js/containers/app.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
-import { TweenLite } from "gsap";
+import React, { Component } from 'react'
+import { TweenLite } from 'gsap'
 
 const img = require('../../images/logo-reactatouille-boilerplate.png')
 
 class App extends Component {
 
-	componentDidMount() {
+  componentDidMount () {
+    TweenLite.fromTo(this.refs.logo, 0.8, { opacity: 0, x: 50 }, { opacity: 1, x: 0, ease: Bounce.easeOut })
+  }
 
-		TweenLite.fromTo(this.refs.logo, 0.8, { opacity: 0, x: 50 }, { opacity: 1, x: 0, ease: Bounce.easeOut })
-
-	}
-
-	render() {
-
-		return (
-			<div className="app">
-				<img ref='logo' src={ img } alt='' />
-			</div>
-		);
-
-	}
+  render () {
+    return (
+      <div className='app'>
+        <img ref='logo' src={img} alt='' />
+      </div>
+    )
+  }
 
 }
 
-export default App;
+export default App
