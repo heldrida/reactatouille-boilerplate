dd14d5e180a0ce2254f47df10e500fecd9393c1b
Helder <heldrida@gmail.com>
2017-02-20 17:32:24 +0000

    spec amend

--- template/test/unit_tests/index.spec.js
+++ template/test/unit_tests/index.spec.js
@@ -1,27 +1,27 @@
 // If a global document does not exists, create one using jsdom.
-require('testdom')('<html><body></body></html>');
+require('testdom')('<html><body></body></html>')
 
-import React from 'react';
-import { expect } from 'chai';
-import { shallow, mount, render } from 'enzyme';
-import App from '../../src/js/containers/app';
-var mock = require('mock-require');
+import React from 'react'
+import { expect } from 'chai'
+import { shallow, mount, render } from 'enzyme'
+import App from '../../src/js/containers/app'
+var mock = require('mock-require')
 
 var TweenLite = mock('gsap', {
-	TweenLite: {
-		fromTo: function () {
-			return null;
-		}
-	}
-});
+  TweenLite: {
+    fromTo: function () {
+      return null
+    }
+  }
+})
 
-describe('The main app component', function() {
-	it('renders without crashing', () => {
-		mount(<App />);
-	});
+describe('The main app component', function () {
+  it('renders without crashing', () => {
+    mount(<App />)
+  })
 
-	it ('should have a class named App', function() {
-		const wrapper = shallow(<App />);
-		expect(wrapper.is('.app')).to.equal(true);
-	});
-});
+  it('should have a class named App', function () {
+    const wrapper = shallow(<App />)
+    expect(wrapper.is('.app')).to.equal(true)
+  })
+})
