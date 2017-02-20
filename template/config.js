da96a603a4720b85cef1ff595fb600bc8db35334
Helder <heldrida@gmail.com>
2017-02-20 15:09:32 +0000

    modified the config js according to standardjs

--- template/config.js
+++ template/config.js
@@ -1,16 +1,16 @@
 var config = {
-	param1: '', // example
-	param2: process.env.NODE_ENV === 'production' ? 'foo' : 'bar', // example
-	git: {
-		remoteList: ['origin', 'heroku'] // add any other remotes here
-	},
-	app_name: 'reactatouille Boilerplate', // your app name here
-	build_name: 'Reactatouille Boilerplate' + ' | ' + (process.env.NODE_ENV || 'development') + ' | ' + '201702062335'
+  param1: '', // example
+  param2: process.env.NODE_ENV === 'production' ? 'foo' : 'bar', // example
+  git: {
+    remoteList: ['origin', 'heroku'] // add any other remotes here
+  },
+  app_name: 'reactatouille Boilerplate', // your app name here
+  build_name: 'Reactatouille Boilerplate' + ' | ' + (process.env.NODE_ENV || 'development') + ' | ' + '201702062335'
 }
 
 // Modified production configuration parameters
 if (process.env.NODE_ENV === 'production') {
-	config.param1 = 'valueProduction';
+  config.param1 = 'valueProduction'
 }
 
-module.exports = config;
+module.exports = config
