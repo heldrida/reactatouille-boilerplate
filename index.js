112ce2d67d93ac158697751ece3fed52bb15cbf8
Helder <heldrida@gmail.com>
2017-02-26 19:48:43 +0000

    included a few notes about the current path envrionment that is set in the include

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+// TODO: refactor and import from /lib directory instead
+// the `./template` is only for the current dev environment
+// has it requires the correct context that is failing at the moment
+// because of the symlink of npm link
 module.exports = {
   loadImage: function (filename) {
     var img
     if (typeof window !== 'undefined' && ({}).toString.call(window) === '[object Window]') {
-      img = require('../../src/images/' + filename) /* require('./template/src/images/' + filename) */
+      // img = require('../../src/images/' + filename) /* require('./template/src/images/' + filename) */
+      img = require('./template/src/images/' + filename)
     } else {
       img = '/assets/images/' + filename
     }
