9a5b447a59e1de3325c3b7f045a99db08b367546
Helder <heldrida@gmail.com>
2017-05-09 15:52:14 +0100

    updated the cmd tool

--- bin/index.js
+++ bin/index.js
@@ -2,59 +2,74 @@
 
 require('babel-polyfill')
 
-var spawn = require('child_process').spawn,
-  chalk = require('chalk'),
-  figlet = require('figlet'),
-  program = require('commander'),
-  Promise = require('bluebird'),
-  fs = Promise.promisifyAll(require('fs-extra')),
-  path = require('path'),
-  rootDir = path.resolve(__dirname, '..'),
-  clear = require('cli-clear'),
-  projectName = ''
+var chalk = require('chalk')
+var figlet = require('figlet')
+var program = require('commander')
+var Promise = require('bluebird')
+var fs = Promise.promisifyAll(require('fs-extra'))
+var path = require('path')
+var rootDir = path.resolve(__dirname, '..')
+var clear = require('cli-clear')
+var version = require('../package.json').version
 
-// Clear the screen
-clear()
-
-// Show the startup banner
-console.log(
-  chalk.magenta(
-    figlet.textSync('Reactatouille', { horizontalLayout: 'full' })
-  ),
-  chalk.yellow.bold('\n' + ' ' + 'Boilerplate CLI'),
-  chalk.yellow('by Punkbit'),
-  '\n',
-  '\n'
-)
-
-// Set command line program parameters
+// Set options
 program
-  .version(require('../package.json').version)
-  .usage('[project name]')
+  .version(version)
+  .option('-n, --new-project <Name...>', 'New project', createNewProject)
+  .option('-c, --create-component <Name...>', 'Create Component', createNewComponent)
   .parse(process.argv)
 
-// Set the name
-projectName = program.args[0]
+// Default to createNewProject fn
+if (program.args.length > 0) {
+  var projectName = program.args[0]
+  if (projectName) {
+    // Clear the screen
+    clear()
+    // Show initial screen
+    startScreen()
+    // Create the project
+    createNewProject(projectName)
+  }
+}
 
-// Show help if user did not provide a project name
-if (!projectName) {
-  program.help()
-} else {
-  //
-  fs.copyAsync(rootDir + '/template', projectName, { clobber: true })
-    .then(function (err) {
-      // Show the initialization text, white space added to text align
-      console.log(chalk.blue(' ' + 'Creating the project directory `' + projectName + '`...'))
-      console.log('\n')
+function startScreen () {
+  // Show the startup banner
+  console.log(
+    chalk.magenta(
+      figlet.textSync('Reactatouille', { horizontalLayout: 'full' })
+    ),
+    chalk.yellow.bold('\n' + ' ' + 'Boilerplate CLI' + ' ' + 'v' + version),
+    chalk.yellow('by Punkbit'),
+    '\n',
+    '\n'
+  )
+}
 
-      if (err) {
-        return console.error(chalk.red.bold(err))
-      } else {
-        console.log(chalk.green(' ' + 'Success! Your project boilerplate is ready!'))
-        console.log(chalk.yellow(' ' + 'Remember to `cd ' + projectName + '` and run the `npm install`'))
-        console.log(chalk.yellow(' ' + 'There, you\'ll find the boilerplate README file containing instructions to run the server, build, etc.'))
-        console.log(chalk.green(' ' + 'Happy coding yo!'))
+function createNewProject (projectName) {
+  // Show help if user did not provide a project name
+  if (!projectName) {
+    program.help()
+  } else {
+    //
+    fs.copyAsync(rootDir + '/template', projectName, { clobber: true })
+      .then(function (err) {
+        // Show the initialization text, white space added to text align
+        console.log(chalk.blue(' ' + 'Creating the project directory `' + projectName + '`...'))
         console.log('\n')
-      }
-    })
+
+        if (err) {
+          return console.error(chalk.red.bold(err))
+        } else {
+          console.log(chalk.green(' ' + 'Success! Your project boilerplate is ready!'))
+          console.log(chalk.yellow(' ' + 'Remember to `cd ' + projectName + '` and run the `npm install`'))
+          console.log(chalk.yellow(' ' + 'There, you\'ll find the boilerplate README file containing instructions to run the server, build, etc.'))
+          console.log(chalk.green(' ' + 'Happy coding yo!'))
+          console.log('\n')
+        }
+      })
+  }
+}
+
+function createNewComponent (name) {
+  console.log('TODO: createNewComponent')
 }
