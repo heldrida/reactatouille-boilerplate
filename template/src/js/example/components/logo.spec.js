a6999b30217c60e01afeb63a48ee6c6a0cfdca81
Helder <heldrida@gmail.com>
2017-05-11 11:51:50 +0100

    removed unwanted files; changed the test command to use jest instead; test files are located along the component file, etc

---
+++ template/src/js/example/components/logo.spec.js
@@ -0,0 +1,17 @@
+/* global test expect it */
+
+import React from 'react'
+import {shallow, mount} from 'enzyme'
+import Logo from './logo'
+
+test('Renders the logo', () => {
+  it('should not crash', () => {
+    mount(<Logo />)
+  })
+  it('should have the correct className', () => {
+    const logo = shallow(
+      <Logo src='foobar.jpg' style='border: 0; width: 10px; height: 10px;' />
+    )
+    expect(logo.is('.logo')).to.equal(true)
+  })
+})
