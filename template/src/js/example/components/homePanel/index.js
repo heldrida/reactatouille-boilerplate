a12e723834d8bcc38ffeec39f34b606d9fecf7af
Helder <heldrida@gmail.com>
2017-05-26 15:34:56 +0100

    updated the pkg version

--- template/src/js/example/components/homePanel/index.js
+++ template/src/js/example/components/homePanel/index.js
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import CtaButton from '../ctaButton'
 import Logo from '../logo'
+import * as Utils from '../../../utils'
+import { loadImage, isBrowser } from 'reactatouille'
 
-const HomePanel = ({image, style, routes, replay}) => (
-  <div className='app home-panel'>
-    <Logo image={image} style={style} />
-    <CtaButton callback={() => replay()} />
-  </div>
-)
+class HomePanel extends Component {
+  constructor (props) {
+    super(props)
+    this.isBrowser = isBrowser()
+    this.state = {
+      style: ''
+    }
+    this.logo = null
+    this.image = loadImage('logo-reactatouille-boilerplate.png')
+    this.style = Utils.helpers.styleObjectParser(this.state.style)
+  }
+
+  componentDidMount () {
+    this.logo = document.querySelector('img.logo')
+    this.isBrowser && this.logoAnimation()
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    if (prevProps.lastUpdated !== this.props.lastUpdated) {
+      this.isBrowser && this.logoAnimation()
+    }
+  }
+
+  logoAnimation () {
+    Utils.animations.onHomePanelReveal({
+      el: this.logo,
+      onComplete: () => {
+        this.setState({
+          style: this.logo.getAttribute('style').replace('z-index', 'zIndex')
+        })
+      }
+    })
+  }
+
+  render () {
+    return (
+      <div className='app home-panel'>
+        <Logo image={this.image} style={this.style} />
+        <CtaButton callback={() => this.props.replay()} />
+      </div>
+    )
+  }
+}
 
 export default HomePanel
--- template/src/js/example/containers/app.js
+++ template/src/js/example/containers/app.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import * as Utils from '../../utils'
-import { loadImage, isBrowser } from 'reactatouille'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { replay } from '../actions'
@@ -8,42 +6,10 @@ import { withRouter, Route } from 'react-router'
 import HomePanel from '../components/homePanel'
 
 class App extends Component {
-  constructor (props) {
-    super(props)
-    this.isBrowser = isBrowser()
-    this.state = {
-      style: ''
-    }
-    this.image = loadImage('logo-reactatouille-boilerplate.png')
-    this.style = Utils.helpers.styleObjectParser(this.state.style)
-  }
-
-  componentDidMount () {
-    this.isBrowser && this.logoAnimation()
-  }
-
-  componentDidUpdate (prevProps, prevState) {
-    if (prevProps.example.lastUpdated !== this.props.example.lastUpdated) {
-      this.isBrowser && this.logoAnimation()
-    }
-  }
-
-  logoAnimation () {
-    const el = document.querySelector('img.logo')
-    Utils.animations.onHomePanelReveal({
-      el: el,
-      onComplete: () => {
-        this.setState({
-          style: el.getAttribute('style').replace('z-index', 'zIndex')
-        })
-      }
-    })
-  }
-
   render () {
     return (
       <div>
-        <HomePanel image={this.image} style={this.style} replay={this.props.replay}>
+        <HomePanel replay={this.props.replay} lastUpdated={this.props.example.lastUpdated}>
           { Array.isArray(this.props.routes) && this.props.routes.map(route => <Route key={route.path} {...route} />) }
         </HomePanel>
       </div>
