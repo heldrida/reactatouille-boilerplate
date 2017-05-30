07a684ad33f5ad8b10c7fac5f717ce244bd7b6b0
Helder <heldrida@gmail.com>
2017-05-30 17:57:15 +0100

    support browse redux dev tool extension by default

--- template/src/js/rootStore.js
+++ template/src/js/rootStore.js
@@ -1,14 +1,18 @@
 import { browserHistory } from 'react-router'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { routerMiddleware } from 'react-router-redux'
 import rootReducer from './rootReducer'
 import thunk from 'redux-thunk'
 
 export default function configureStore (initialState) {
-  const store = createStore(
-    rootReducer,
+  const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
+  const enchancers = composeEnhancers(
     applyMiddleware(thunk),
     applyMiddleware(routerMiddleware(browserHistory))
+  )
+  const store = createStore(
+    rootReducer,
+    enchancers
   )
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
